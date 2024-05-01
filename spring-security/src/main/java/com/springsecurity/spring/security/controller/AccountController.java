32669a040d27a05edd5dd8a66205458ee4725e54
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2024-05-01 09:03:43 +0530

    created new project

---
+++ spring-security/src/main/java/com/springsecurity/spring/security/controller/AccountController.java
@@ -0,0 +1,14 @@
+package com.springsecurity.spring.security.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/account")
+@CrossOrigin
+public class AccountController {
+
+    @GetMapping("/my-account")
+    public String getAccountDetails(){
+        return "here are the account details";
+    }
+}
