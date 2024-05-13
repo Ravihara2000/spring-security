be46e105ec05c6cce5cea9359c538472ca864177
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2024-05-13 12:05:14 +0530

    tool bar added in frontend

--- spring-security/src/main/java/com/springsecurity/spring/security/config/projectSecurityConfig.java
+++ spring-security/src/main/java/com/springsecurity/spring/security/config/projectSecurityConfig.java
@@ -3,6 +3,8 @@ package com.springsecurity.spring.security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,13 +15,14 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableGlobalMethodSecurity
 public class projectSecurityConfig {
     //match the path which should secure and not
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/v1/account/my-account").hasAnyAuthority("admin")
+                .antMatchers("/api/v1/account/my-account").hasAnyAuthority("admin")//.hasRole also can use-->.hasRole("ADMIN")
                 .antMatchers( "/api/v1/loan/my-loan").hasAnyAuthority("user")
                 .antMatchers("/api/v1/notice/my-notice", "/api/v1/user/register").permitAll()
                 .and()
